9ad66a8ffc9d9a58a7fa6c6ca02bdf0d1a2ce80b
doctoron <ron.antonio@me.com>
2019-02-12 15:05:04 -0500

    Closer Still

--- keys.js
+++ keys.js
@@ -1,22 +1,11 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  // id: process.env.SPOTIFY_ID,
-  // secret: process.env.SPOTIFY_SECRET
   id: "8d69f921d0e84a538daa043c6b1869c9",
   secret: "5349065ce99c4170b47a2cffe4025ac3"
 }
 
-// exports.omdbApi = {
-// omdbApi = require('omdb-client');
-// movieQueryURL = "https://www.omdbapi.com/?t=";
-// exports.omdbApi = {
-
-// var params = {
-    // apiKey: '&y=&plot=short&apikey=trilogy',
-//     title: 'Terminator',
-//     year: 2012
-// }
-// omdbApi.get(params, function (err, data) {
-// process response...  
-// });
+exports.omdbApiKey = {
+    id: process.env.OMDB_APIKEY
+    // '71e5665f'
+}
--- liri.js
+++ liri.js
@@ -1,111 +1,139 @@
 require("dotenv").config();
 // Use key data from keys.js
 let keys = require('./keys.js');
-let request = require('request');
+let axios = require('axios');
 let Spotify = require('node-spotify-api');
 let spotify = new Spotify(keys.spotify);
-let searchItem = process.argv.slice(3).join(' '); 
-let command = process.argv[2];
-let bandsintown = require('bandsintown')('faswega');
-let axios = require('axios');
 let moment = require('moment');
 let fs = require('fs');
+let command = process.argv[2];
+let searchItem = process.argv.slice(3).join(' ');
+// let defaultMovie = "Mr. Nobody";
+// let defaultSong = 'Happy'
+// let bandsintown = require('bandsintown')('faswega');
+// let omdbApi = './keys.apiKey'
+// let inquirer = require('inquirer');
+// let request = require('request');
 
-bandsintown
-    .getArtistEventList(searchItem)
-    .then(function (events) {
-        // return array of events
-    });
-
-
-// console.log(`process.argv was fired in cliText ${command}`);
-// console.log(`'searchItem' was fired: ${searchItem}`);
 
 let choice = function (command, searchItem) {
+    console.log(command, searchItem);
+
     switch (command) {
         case 'movie-this':
-            console.log(`movie-this command was fired`)
-            omdbData(searchItem);
-
-            break;
+        if (searchItem === " ") {
+            console.log("Tell me the name of your movie?")
+                break;
+            } else {
+                omdbData(searchItem);
+                break;
+            }
 
         case 'spotify-this-song':
-            console.log(`spotify-this was fired`)
-            spotifyData(searchItem);
-
-            break;
+            if (searchItem === " ") {
+                console.log("Tell me the name of your song?")
+                break;
+            } else {
+                spotifyData(searchItem);
+                break;
+            }
 
         case 'concert-this':
-            console.log('concert-this was fired')
-            concertData();
-
-            break;
+            if (searchItem === " ") {
+                console.log('What band do you wish to follow?')
+                break;
+            } else {
+                concertData(searchItem);
+                break;
+            }
 
         case 'do-what-it-says':
-            console.log(`do-what-it-says was just fired`);
-            // justDoIt();
-
-            break;
+            if (searchItem === " ") {
+                console.log("Tell me the name of your song?")
+                break;
+            } else {
+                justDoIt();
+                break;
+            }
 
         default:
             console.log(`What are you trying to do?`);
-
             break;
     }
 }
 
 let concertData = (searchItem) => {
-
-    axios.get(`https://rest.bandsintown.com/artists/${searchItem}?app_id=faswega`)
-        .then(function (response) {
-            console.log(response);
-        })
+    let bandsURL = `https://rest.bandsintown.com/artists/${searchItem}/events?app_id=codingbootcamp`;
+    console.log(bandsURL);
+    axios.get(bandsURL).then(function (response) {
+        console.log(`Seaching for concerts by ${searchItem}`)
+        let bandData = response.data;
+        for (let i = 0; i < bandData.length; i++) {
+            let bandEvents = bandData[0];
+            let concerts = [
+                'Name of the venue: ' + bandEvents.venue.name,
+                'Venue location: ' + bandEvents.venue.city + bandEvents.venue.region,
+                'Date of the Event: ' + moment(bandEvents.venue.datetime).format("MM/DD/YYYY")
+            ]
+            console.log(concerts);
+        }
+    })
         .catch(function (error) {
             console.log(error);
+        });
+};
+
+let spotifyData = (song) => {
+    spotify
+        .search({ type: 'track', query: song })
+        .then(function (response) {
+            let song = response.tracks.items[0];
+            let songItems = [
+                'Artist: ' + song.album.artists[0].name,
+                'Song Name: ' + song.name,
+                'Song Preview: ' + song.preview_url,
+                'Album: ' + song.album.name
+            ]
+            console.log(songItems);
+
+        })
+        .catch(function (err) {
+            console.log('Error occurred: ' + err);
         });
 }
 
 
-let spotifyData = (searchItem) => {
-
-    spotify.search({ type: 'track', query: searchItem }, function (err, data) {
-        if (err) {
-            return console.log('Error occurred: ' + err);
-        }
-
-        console.log(data.artists);
-        console.log(data.album);
-        console.log(data.tracks);
-    });
-
-}
-
-
-let omdbData = (searchItem) => {
-    var omdbURL = "http://www.omdbapi.com/?t=$" + searchItem + "&plot=short&tomatoes=true&y=&plot=short&apikey=trilogy";
+let omdbData = (movie) => {
+    let omdbURL = `http://www.omdbapi.com/?t=$${movie}&plot=short&tomatoes=true&apikey=trilogy`;
 
     axios.get(omdbURL)
         .then(function (response) {
-            // console.log(response);
-            var body = response.data;
+            let body = response.data;
             // console.log(body.Title);
-            console.log("Title: " + body.Title);
-            console.log("Release Year: " + body.Year);
-            console.log("IMdB Rating: " + body.imdbRating);
-            console.log("Country: " + body.Country);
-            console.log("Language: " + body.Language);
-            console.log("Plot: " + body.Plot);
-            console.log("Actors: " + body.Actors);
-            console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
-            console.log("Rotten Tomatoes URL: " + body.tomatoURL);
-
+            newFunction(body);
         })
         .catch(function (error) {
             console.log(error);
         });
 
 }
+
 let doThis = function (arg1, arg2) {
     choice(arg1, arg2);
 }
+
+// Let's begin here
 doThis(command, searchItem);
+
+function newFunction(body) {
+    console.log("Title: " + body.Title);
+    console.log("Release Year: " + body.Year);
+    console.log("IMdB Rating: " + body.imdbRating);
+    console.log("Country: " + body.Country);
+    console.log("Language: " + body.Language);
+    console.log("Plot: " + body.Plot);
+    console.log("Actors: " + body.Actors);
+    console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
+    console.log("Rotten Tomatoes URL: " + body.tomatoURL);
+
+}
